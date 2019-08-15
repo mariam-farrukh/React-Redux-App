a446618e609d9c98f3b7425aa236e36dd1b7e039
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-15 17:36:10 -0500

    Changed to upcoming launches

--- redux-project/src/store/actions/index.js
+++ redux-project/src/store/actions/index.js
@@ -8,7 +8,7 @@ export const getData = () => {
   return dispatch => {
     dispatch({ type: FETCH_LAUNCH_DATA_START });
     axios
-      .get('https://api.spacexdata.com/v3/launches/past')
+      .get('https://api.spacexdata.com/v3/launches/upcoming')
       .then(res => {
         console.log(res);
         dispatch({ type: FETCH_LAUNCH_DATA_SUCCESS, payload: res.data });
